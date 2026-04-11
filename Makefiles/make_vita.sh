c199c764dfd489d0d9df7ad51517a527f08eb26d
Matt19220 <matthew.gallon0912@gmail.com>
2026-04-11 21:43:55 +0200

    Bugfixes 9

--- Makefiles/make_vita.sh
+++ Makefiles/make_vita.sh
@@ -1,5 +1,10 @@
 export VITASDK=/usr/local/vitasdk
+export PATH=$VITASDK/bin:$PATH
 export PKG_CONFIG_PATH=$VITASDK/arm-vita-eabi/lib/pkgconfig
+# arm-vita-eabi-pkg-config is the cross-compiling pkg-config for the Vita sysroot.
+# We must pass it explicitly to cmake; otherwise cmake uses the host pkg-config
+# which knows nothing about vitasdk and fails PKG_SEARCH_MODULE for SDL2_mixer/ttf.
+export PKG_CONFIG=$VITASDK/bin/arm-vita-eabi-pkg-config
 rm -rf build_vita
 mkdir build_vita
 cd build_vita
@@ -8,5 +13,6 @@ cmake ../ \
   -DVITA=ON \
   -DFORCE_DRAW_BG=ON \
   -DFORCE_DRAW_OVERLAY=ON \
-  -DCMAKE_PREFIX_PATH=$VITASDK/arm-vita-eabi
+  -DCMAKE_PREFIX_PATH=$VITASDK/arm-vita-eabi \
+  -DPKG_CONFIG_EXECUTABLE=$VITASDK/bin/arm-vita-eabi-pkg-config
 make
